988cac877c301b046bd36b97510cf94c7124ac47
Larry Guerra <larryg@softwise.com>
2021-07-22 19:19:41 -0600

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
+  // styles: [`
+  // h3 {
+  //   color: dodgerblue;
+  // }
+  // `]
 })
+
 export class AppComponent {
-  title = 'my-first-ng-app';
 }
